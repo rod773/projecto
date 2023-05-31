60547ffbac1c059f7efe4b86a5173e939c0b1891
rod773 <rodrigoarcadio1@gmail.com>
2023-05-31 22:42:55 +0200

    modificaciones web api

---
+++ backend/WebApi/Model/Usuario.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApi.Models
+{
+    public class Usuario
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Nombre { get; set; }
+
+        [Required]
+        public string Email { get;}
+
+    }
+}
