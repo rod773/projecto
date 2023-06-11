795517ca337c2ca8c913cbab6b62c625b3c2287c
rod773 <rodrigoarcadio1@gmail.com>
2023-06-11 18:00:27 +0200

    pagina acerca de

--- frontend/rifa/src/components/Acercade.vue
+++ frontend/rifa/src/components/Acercade.vue
@@ -1,7 +1,27 @@
 <template>
-  <div>
-    <h1 class="text-h2">Acerca De</h1>
-  </div>
+  <v-row>
+    <v-col cols="12" class="d-flex justify-center">
+      <h1 class="text-h2">Acerca De</h1>
+    </v-col>
+  </v-row>
+  <v-col cols="12">
+    <v-img src="@/assets/img/acercade.png" class="rounded-xl"></v-img>
+  </v-col>
+  <v-col>
+    <v-p class="text-h5 text-light ma-10">
+      Los premios juegan un gran papel en el costo de los boletos de las rifas y
+      el número de participantes que atraerás. Los premios mejores aumentan las
+      chances de una exitosa rifa navideña. Escoge premios que se adapten al
+      tema de la Navidad. Una rifa 50/50 funciona bien cerca de las fiestas, ya
+      que el premio puede ser destinado a regalos para Navidad. El ganador de la
+      rifa se lleva la mitad del dinero recaudado mientras que el grupo
+      organizador se queda con la otra mitad. Busca donaciones de negocios
+      locales que puedan servir como regalos de Navidad. Algunas ideas incluyen
+      certificados para el ingreso a baños termales, una estadía por un fin de
+      semana en una posada, un sistema de video-juegos, vinos de una tienda
+      local o canastas Navideñas de distintos locales.
+    </v-p>
+  </v-col>
 </template>
 
 <script setup lang="ts"></script>
